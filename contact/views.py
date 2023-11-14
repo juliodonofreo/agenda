efcbd45300de5a16f2790ef0a77aafeac202fd1e
Julio Donofreo <julio.bibico@hotmail.com>
2023-11-14 15:09:22 -0300

    Criação de testes na index e configurações

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    
+    context = {
+        "texto": "Contact inicial"
+    }
+    return render(request, "contact/index.html", context)
