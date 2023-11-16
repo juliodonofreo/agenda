c68ec5ca1418b0b808d56e36aaddd36a628befc7
Julio Donofreo <julio.bibico@hotmail.com>
2023-11-16 13:55:11 -0300

    Criação do model de contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_nome = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+    
