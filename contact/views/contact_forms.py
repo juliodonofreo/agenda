33795a43e8e594e1007c263872aa65975f931792
Julio Donofreo <julio.bibico@hotmail.com>
2023-11-28 14:12:01 -0300

    Criando chave CSRF e testando funcionamento do formulario

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,6 +1,8 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
-from django.core.paginator import Paginator
+from django.views.decorators.http import require_POST
+
 
 from contact import models
 
@@ -8,10 +10,17 @@ from contact import models
 # Create your views here.
 def create(request):
 
-    
-    
+    print(request.method)
+
+    if(request.method == "POST"):
+        post = request.POST
+        
+        for chave, valor in post.items():
+            print(chave, valor)
+
     context = {
         
     }
     
     return render(request, "contact/create.html", context)
+
