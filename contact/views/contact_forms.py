f04ceff86a5461b405bf3010b4f08a99d9d55331
Julio Donofreo <julio.bibico@hotmail.com>
2023-11-30 12:27:55 -0300

    Criando ModelForm

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,26 +1,40 @@
+from django import forms
+from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator
 from django.db.models import Q
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import redirect, render
 from django.views.decorators.http import require_POST
 
-
 from contact import models
+from contact.models import Contact
 
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ["first_name", "last_nome", "phone"]
+        
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        
+        self.add_error(
+            "first_name",
+            ValidationError("Mensagem de erro", code="invalid")
+            );
 
 # Create your views here.
 def create(request):
-
-    print(request.method)
-
-    if(request.method == "POST"):
-        post = request.POST
-        
-        for chave, valor in post.items():
-            print(chave, valor)
-
+    if request.method == "POST":
+        context = {
+            "form": ContactForm(data=request.POST)
+        }
+    
+        return render(request, "contact/create.html", context)
+    
     context = {
-        
+        "form": ContactForm(),
     }
     
     return render(request, "contact/create.html", context)
+
 
