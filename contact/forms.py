4fa7f555bc180fba8fb4cff845833329247e6393
Julio Donofreo <julio.bibico@hotmail.com>
2023-12-12 12:13:44 -0300

    Erro personalizado

--- contact/forms.py
+++ contact/forms.py
@@ -28,7 +28,11 @@ class ContactForm(forms.ModelForm):
     def clean(self):
         cleaned_data = self.cleaned_data
         
-        self.add_error(
-            "first_name",
-            ValidationError("Mensagem de erro", code="invalid")
-            );
+        first_name = cleaned_data.get("first_name", "")
+        last_nome = cleaned_data.get("last_nome", "")
+        
+        if first_name == last_nome:
+            self.add_error(
+                "first_name",
+                ValidationError("Primeiro nome não pode ser igual ao segundo", code="invalid")
+            )
